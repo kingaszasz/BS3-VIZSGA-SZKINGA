6818b28bf01e831bb9a9e5fed8988bd0b706b303
Szász Kinga <kingus77@gmail.com>
2018-03-16 12:06:35 +0100

    contact sectio nice

--- js/ownJs.js
+++ js/ownJs.js
@@ -9,18 +9,3 @@ $(document).ready(function () {
         }
     });
 });
-
-var c = document.getElementById("myCanvas");
-var icon = document.getElementById("mobile");
-var ctx = c.getContext("2d");
-
-
-ctx.font = "30px Verdana";
-// Create gradient
-var gradient = ctx.createLinearGradient(0, 0, c.width, 0);
-gradient.addColorStop("0", "magenta");
-gradient.addColorStop("0.5", "blue");
-gradient.addColorStop("1.0", "red");
-// Fill with gradient
-ctx.fillStyle = gradient;
-ctx.fillText(icon, 10, 90);
